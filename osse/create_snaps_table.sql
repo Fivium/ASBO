dbfe1c0872a20e6476744c0ccba2c93d9538ea50
thomasdale <tom.dale@fivium.co.uk>
2016-01-13 10:31:25 +0000

    Update create_snaps_table.sql
    Extra details added

--- osse/create_snaps_table.sql
+++ osse/create_snaps_table.sql
@@ -10,21 +10,45 @@
 --
 DROP TABLE dbamgr.session_snaps
 /
-CREATE TABLE dbamgr.session_snaps
-( dbid             NUMBER
-, instance_number  NUMBER
-, sample_time      DATE
-, session_type     VARCHAR2(64 BYTE)
-, session_state    VARCHAR2(64 BYTE)
-, wait_class       VARCHAR2(64 BYTE)
-, event            VARCHAR2(64 BYTE)
-, sql_id           VARCHAR2(64 BYTE)
-, session_id       VARCHAR2(64 BYTE)
-, user_id          VARCHAR2(64 BYTE)
-, program          VARCHAR2(64 BYTE)
-, module           VARCHAR2(64 BYTE)
-, blocking_session NUMBER
-, machine          VARCHAR2(64 BYTE)
+CREATE TABLE dbamgr.session_snaps( 
+  DBID                          NUMBER,
+  INSTANCE_NUMBER               NUMBER,
+  SAMPLE_TIME                   DATE,
+  SESSION_TYPE                  VARCHAR2(64 BYTE),
+  SESSION_STATE                 VARCHAR2(64 BYTE),
+  WAIT_CLASS                    VARCHAR2(64 BYTE),
+  EVENT                         VARCHAR2(64 BYTE),
+  SQL_ID                        VARCHAR2(64 BYTE),
+  SESSION_ID                    VARCHAR2(64 BYTE),
+  USER#                         VARCHAR2(64 BYTE),
+  USERNAME                      VARCHAR2(128 BYTE),
+  PROGRAM                       VARCHAR2(64 BYTE),
+  MODULE                        VARCHAR2(64 BYTE),
+  BLOCKING_SESSION              NUMBER,
+  MACHINE                       VARCHAR2(64 BYTE),
+  WAIT_CLASS#                   INTEGER,
+  EVENT#                        INTEGER,
+  WHAT_SESSION_IS_DOING_CLASS   VARCHAR2(64 BYTE),
+  WHAT_SESSION_IS_DOING_CLASS#  INTEGER,
+  BLOCKER                       VARCHAR2(64 BYTE),
+  CLIENT_INFO                   VARCHAR2(100 BYTE),
+  LOGON_TIME                    DATE,
+  PREV_SQL_ID                   VARCHAR2(64 BYTE),
+  PLSQL_ENTRY_OBJECT_ID         VARCHAR2(64 BYTE),
+  PLSQL_ENTRY_SUBPROGRAM_ID     VARCHAR2(64 BYTE),
+  PLSQL_OBJECT_ID               VARCHAR2(64 BYTE),
+  PLSQL_SUBPROGRAM_ID           VARCHAR2(64 BYTE),
+  TYPE                          VARCHAR2(64 BYTE),
+  SERIAL#                       NUMBER,
+  P1                            NUMBER,
+  P1TEXT                        VARCHAR2(64 BYTE),
+  P1RAW                         RAW(8),
+  P2                            NUMBER,
+  P2TEXT                        VARCHAR2(64 BYTE),
+  P2RAW                         RAW(8),
+  P3                            NUMBER,
+  P3TEXT                        VARCHAR2(64 BYTE),
+  P3RAW                         RAW(8)	
 )
 /
 CREATE INDEX dbamgr.session_snaps_idx1 ON dbamgr.session_snaps(sample_time)
