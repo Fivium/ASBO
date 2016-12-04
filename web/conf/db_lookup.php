8e9251d16af5f9287f635098479dfbd6437105a7
thomasdale <tom.dale@fivium.co.uk>
2016-12-04 16:13:10 +0000

    second example added

--- web/conf/db_lookup.php
+++ web/conf/db_lookup.php
@@ -13,8 +13,16 @@ $db_look_up_xml=<<<EOXML
         <user>DBA USER OR OWNER OF SNAPING TABLES (eg dbamgr)</user>
         <pw>PW (eg password1234)</pw>
         <snaps_table>SNAPS TABLE IF NOT ACTIVE_SESSION_HISTORY (eg dbamgr.session_snaps)</snaps_table>
-        <use_local_awr_tables>TRUE IF NOT :wACTIVE_SESSION_HISTORY (eg TRUE)</use_local_awr_tables>
+        <use_local_awr_tables>TRUE IF NOT ACTIVE_SESSION_HISTORY (eg TRUE)</use_local_awr_tables>
     </db>    
+    <db name="example2)">
+        <display_name>Example DB</display_name>
+        <conn_str>192.168.0.1:1521/xe.oracle.docker)</conn_str>
+        <user>dbamgr</user>
+        <pw>dbamgr</pw>
+        <snaps_table>dbamgr.session_snaps</snaps_table>
+        <use_local_awr_tables>TRUE</use_local_awr_tables>
+    </db>                
 </databases>
 EOXML;
 ?>
