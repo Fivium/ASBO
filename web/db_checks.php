a1e51f75f0c98a8034c3e3c9dce8f4ea8f2f00f1
thomasdale <tom.dale@fivium.co.uk>
2017-01-31 14:47:05 +0000

    added data guard check for ee edition

--- web/db_checks.php
+++ web/db_checks.php
@@ -1,11 +1,48 @@
 <?php
 #
-# $Id$
+# $Id: //Infrastructure/GitHub/Database/asbo/web/db_checks.php#7 $
 #
 if( !isset( $just_checks ) ) $just_checks = 0;
 
-if( $just_checks !== 1 ) include 'start.php';
+if(!function_exists("display")) {
+    function display($cur,$db_obj,&$any_crits){
 
+        #
+        # Buffer the check output
+        #
+        ob_start();
+
+        if( u::request_val('html') ){
+           $db_obj->html_results( $cur );
+        }else{
+            $db_obj->text_results( $cur );
+        }
+
+        $html_buffer = ob_get_contents();
+        ob_end_clean();
+        #
+        # Only criticals
+        #
+        if( u::request_val('suppress_ok') ){
+            #
+            # Any criticals?
+            #
+            if( strpos( $html_buffer, 'CRITICAL' ) ){
+                #
+                # Display the check result
+                #
+                echo $html_buffer;
+                $any_crits = 1;
+            }
+        }else{
+            echo $html_buffer;
+        }
+    }
+}
+
+if( $just_checks !== 1 ) include 'start.php';
+    $any_crits=0;
+    $crit_count=0;
     #
     # Check full backup status
     #
@@ -13,25 +50,44 @@ if( $just_checks !== 1 ) include 'start.php';
     $binds   = '';
     $binds[] = array( ":db_str", $db );
     $cur = $db_obj->exec_sql( $sql, $binds );
-    $db_obj->html_results( $cur );
+    display( $cur, $db_obj, $any_crits );
+    $crit_count += $any_crits;
     #
     # Database space
     #
     $sql = file_get_contents("./sql/tablespace_check.sql");
     $cur = $db_obj->exec_sql( $sql, $binds );
-    $db_obj->html_results( $cur );
+    display( $cur, $db_obj, $any_crits );
+    $crit_count += $any_crits;
     #
     # Standby status
     #
     if( $db_obj->using_dbsync() ){
+        $cur = $db_obj->exec_sql( 'BEGIN dbamgr.dbsync.process_logs; END;', '' );
         $binds   = '';
-        $binds[] = array( ":db_str", $db );    
+        $binds[] = array( ":db_str", $db );
         $sql = file_get_contents( "./sql/standby_check.sql" );
         $cur = $db_obj->exec_sql( $sql, $binds );
-        $db_obj->html_results( $cur );
-     }
+        display( $cur, $db_obj, $any_crits );
+        $crit_count += $any_crits;
+    }
+    #
+    # Datagaurd?
+    #
+    $dg_check = $db_obj->single_rec('select \'YES\' using_dg from v$parameter where name=\'log_archive_config\' and length(value)>4 and upper(value) not like \'%NODG_CONFIG%\' ');
 
+    if( isset( $dg_check->USING_DG ) ){
+
+        $sql = file_get_contents("./sql/datagaurd_check.sql");
+        $cur = $db_obj->exec_sql( $sql );
+        display( $cur, $db_obj, $any_crits );
+        $crit_count += $any_crits;
+    }
     u::flush();
+
+    if( u::request_val('suppress_ok') and $crit_count === 0 ){
+       u::p(' - All checks OK');
+    }
 
 if( $just_checks !== 1 ) include 'end.php';
 ?>
