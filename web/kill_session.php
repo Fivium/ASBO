adbb14b37a0168fac198d682bb706d75030e9c61
thomasdale <tom.dale@fivium.co.uk>
2017-09-18 10:25:18 +0100

    Update kill_session.php

--- web/kill_session.php
+++ web/kill_session.php
@@ -1,6 +1,6 @@
 <?php
 #
-# $Id$
+# $Id: //Infrastructure/GitHub/Database/asbo/web/kill_session.php#3 $
 #
 include 'start.php';
 
@@ -8,10 +8,22 @@ $sid    = u::request_val('sid');
 $serial = u::request_val('serial');
 
 if ( $sid and $serial ){
-  $kill_sql = "alter system kill session '$sid,$serial'";
+
+  $kill_sql = "
+BEGIN
+  FOR l_rec IN (SELECT sid,serial# FROM v\$session WHERE sid = :sid AND serial# = :serial) LOOP
+    EXECUTE IMMEDIATE 'alter system kill session '||CHR(39)||l_rec.sid||','||l_rec.serial#||CHR(39);
+  END LOOP;
+END;
+";
+
   p('SQL : '.$kill_sql);
-  $cur = $db_obj->exec_sql( $kill_sql );
-  p(' DONE!');
+  $binds = '';
+  $binds[] = array(':sid',$sid);
+  $binds[] = array(':serial',$serial);
+
+  $cur = $db_obj->exec_sql( $kill_sql, $binds );
+  p("\n\n\n<br><br>DONE!");
 }else{
   p( 'Pamarms not given' );
 }
