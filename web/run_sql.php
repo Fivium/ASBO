9b6d62c84d485a522d0cfe9cfa505627c7c089f9
thomasdale <tom.dale@fivium.co.uk>
2017-09-20 10:23:52 +0100

    white list added

--- web/run_sql.php
+++ web/run_sql.php
@@ -16,58 +16,76 @@ $param3      = u::request_val( 'param3'      );
 $param4      = u::request_val( 'param4'      );
 $dbms_output = u::request_val( 'dbms_output' );
 
-u::p("<!--");
-for($i=1;$i<=4;$i++){
-    $param_name="param$i";
-    u::p("Param$i : ".$$param_name);
-}
-u::p("-->");
+$allowed_sql_files = array(
+    'advisor_executions',
+    'create_outline',
+    'last_login_check',
+    'logfile_sizes',
+    'system_stats',
+    'unindexed_fk'
+);
 
-p("<title>Run : $sql_file</title>");
+if(in_array($sql_file,$allowed_sql_files)){
 
-$sql = file_get_contents( "./sql/".$sql_file.".sql" );
-#$sql = str_replace( "'", "\'", $sql );
-#
-# Schema needed?
-#
-if( $schema = u::request_val( 'schema' ) ){
-    $sql = str_replace( '__SCHEMA__', $schema, $sql );
-}
+	u::p("<!--");
+	for($i=1;$i<=4;$i++){
+	    $param_name="param$i";
+	    u::p("Param$i : ".$$param_name);
+	}
+	u::p("-->");
 
-#p($sql);
-function var_set_to_something($var){
-   
-     if( $var or $var === '0' or $var === 0 ){
-         return 1;
-     }else{
-         return 0;
-     }     
-}
+	p("<title>Run : $sql_file</title>");
 
-$binds   = '';
-if( var_set_to_something($param1) ) $binds[] = array( ":param1"  , $param1   );
-if( var_set_to_something($param2) ) $binds[] = array( ":param2"  , $param2   );
-if( var_set_to_something($param3) ) $binds[] = array( ":param3"  , $param3   );
-if( var_set_to_something($param4) ) $binds[] = array( ":param4"  , $param4   );
+	$sql = file_get_contents( "./sql/".$sql_file.".sql" );
+	#$sql = str_replace( "'", "\'", $sql );
+	#
+	# Schema needed?
+	#
+	if( $schema = u::request_val( 'schema' ) ){
+	    $sql = str_replace( '__SCHEMA__', $schema, $sql );
+	}
 
-u::p("<!--");
-foreach($binds as $bind_pair){
-    u::p($bind_pair[0].' : '.$bind_pair[1]);
-}
-u::p("-->");
+	u::p("<!--");
+	p($sql);
+	u::p("-->");
 
-if( $dbms_output ) $db_obj->dbms_output_on();
 
-$cur = $db_obj->exec_sql( $sql, $binds );
-#
-# SQL results or dbms_output
-#
-if( $dbms_output ){
-    $db_obj->get_dbms_output();
-    oci_free_statement($cur);
-}else{
-    $db_obj->html_results( $cur );
-}
+	function var_set_to_something($var){
+	   
+	     if( $var or $var === '0' or $var === 0 ){
+	         return 1;
+	     }else{
+	         return 0;
+	     }     
+	}
+
+	$binds   = array();
+	if( var_set_to_something($param1) ) $binds[] = array( ":param1"  , $param1   );
+	if( var_set_to_something($param2) ) $binds[] = array( ":param2"  , $param2   );
+	if( var_set_to_something($param3) ) $binds[] = array( ":param3"  , $param3   );
+	if( var_set_to_something($param4) ) $binds[] = array( ":param4"  , $param4   );
+
+	u::p("<!--");
+	foreach($binds as $bind_pair){
+	    u::p($bind_pair[0].' : '.$bind_pair[1]);
+	}
+	u::p("-->");
+
+	if( $dbms_output ) $db_obj->dbms_output_on();
+
+	$cur = $db_obj->exec_sql( $sql, $binds );
+	#
+	# SQL results or dbms_output
+	#
+	if( $dbms_output ){
+	    $db_obj->get_dbms_output();
+	    oci_free_statement($cur);
+	}else{
+	    $db_obj->html_results( $cur );
+	}
+
+	}
 
 include 'end.php';
 ?>
+
