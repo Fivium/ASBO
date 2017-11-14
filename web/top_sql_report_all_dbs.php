f9ad820512072b97df2ccf00a8944f70ec56f331
thomasdale <tom.dale@fivium.co.uk>
2017-11-14 14:50:18 +0000

    run top sql report across all dbs

---
+++ web/top_sql_report_all_dbs.php
@@ -0,0 +1,51 @@
+<?php
+#
+# $Id: //Infrastructure/GitHub/Database/asbo/web/top_sql_report.php#3 $
+#
+# Display some sql stats
+#
+if( !isset( $just_report ) ) $just_report = 0;
+if( $just_report !== 1 ) include 'start.php';
+
+$db_obj->dbms_output_on();
+
+$sql = file_get_contents( "./sql/top_sql_report.sql" );
+
+$start = u::request_val( 'start' );
+$end   = u::request_val( 'end'   );
+
+$binds = '';
+
+#
+# Using our own history tables?
+#
+
+include 'subs_for_history_tables.php';
+$binds[] = array( ":os_start_snap", '0' );
+$binds[] = array( ":os_end_snap"  , '0' );
+
+#p('---SQL------------------');
+#p($sql);
+#p('---SQL------------------');
+
+p("<title>Quick SQL Report for $db</title>");
+
+$binds[] = array( ":from_date_str"          , $start                             );
+$binds[] = array( ":to_date_str"            , $end                               );
+$binds[] = array( ":order_by_id"            , u::request_val( 'order_by_id', 1 ) );
+$binds[] = array( ":db_str"                 , u::request_val( 'db'             ) );
+#
+# These binds should be give as options in the page - Todo
+#
+#$binds[] = array( ":ignore_plsql"           , 1                         );
+#$binds[] = array( ":ignore_support_activity", 1                         );
+#$binds[] = array( ":ignore_reportmgr"       , 1                         );
+#$binds[] = array( ":result_count"           , 20                        );
+
+$cur     = $db_obj->exec_sql( $sql, $binds );
+
+$db_obj->get_dbms_output();
+
+if( $just_report !== 1 ) include 'end.php';
+
+?>
