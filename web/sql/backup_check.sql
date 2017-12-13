2558209a8395ac06de2bd438e286d3987dd9c77e
IM4rk <34507571+IM4rk@users.noreply.github.com>
2017-12-13 10:44:02 +0000

    Update backup_check.sql
    - Added check for missing backups when the RMAN table does not return any rows

--- web/sql/backup_check.sql
+++ web/sql/backup_check.sql
@@ -1,40 +1,54 @@
 --
--- $Id: //Infrastructure/GitHub/Database/asbo/web/sql/backup_check.sql $
+-- $Id: //Infrastructure/Database/scripts/oav/sql/backup_check.sql#2 $
 --
-WITH 
+WITH
   backup_details
 AS
 (
   SELECT
-    TO_CHAR(start_time,'dd-mon hh24:mi:ss') bk_start
-  , TO_CHAR(end_time  ,'dd-mon hh24:mi:ss') bk_end
-  , (end_time - start_time) * 3600 * 24     duration_mins
+   TO_CHAR(start_time,'dd-mon hh24:mi:ss') bk_start
+  ,TO_CHAR(end_time  ,'dd-mon hh24:mi:ss') bk_end
+  ,(end_time - start_time) * 3600 * 24     duration_mins
   , status full_backup_status
   , CASE
       WHEN status = 'COMPLETED WITH WARNINGS' OR status = 'RUNNING WITH WARNINGS' THEN 'WARNING'
-      WHEN status NOT IN ('COMPLETED','RUNNING') THEN 'CRITICAL'
+      WHEN status NOT IN ('COMPLETED','RUNNING') then 'CRITICAL'
       ELSE 'OK'
     END bk_status
-  --, '<a href=rman_progress.php?db='||:db_str||'>Details</a>' rman_details
   , '<a href=rman_log.php?db='||:db_str||'&session_recid='||session_recid||'>log</a>' backup_log
-  FROM 
-    v$rman_backup_job_details
-  WHERE 
-    input_type = 'DB FULL' AND 
-    start_time > SYSDATE - 2 
-  ORDER BY 
-    start_time DESC
+  FROM
+    v$rman_backup_job_details 
+  WHERE
+    input_type = 'DB FULL' AND
+    start_time > SYSDATE - 2
+  
+  UNION ALL
+   
+   select 
+     'NO BACKUP'  bk_start
+    , NULL  bk_end
+    , NULL  duration_mins
+    , NULL  full_backup_status
+    , 'CRITICAL'  bk_status
+    , NULL backup_log
+    from dual 
+   where not exists 
+       (  
+            SELECT * FROM v$rman_backup_job_details WHERE
+            input_type = 'DB FULL' AND
+            start_time > SYSDATE - 2
+       )
 )
-SELECT
+select 
   bk_start
 , bk_end
 , full_backup_status
---, rman_details
---, backup_log
+, bk_status
+, backup_log
 , TO_CHAR(TRUNC(duration_mins/3600),'FM9900')       || 'hr '   ||
   TO_CHAR(TRUNC(MOD(duration_mins,3600)/60),'FM00') || 'mins ' ||
   TO_CHAR(MOD(duration_mins,60),'FM00')             || 'secs'  duration
-, bk_status
-FROM
-  backup_details bd
-
+From  backup_details bd
+WHERE
+ROWNUM < 2
+order by bk_start
