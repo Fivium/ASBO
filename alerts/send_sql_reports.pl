deed74a4a3b5b2f53178a0136cdf9d08ec464a45
IM4rk <34507571+IM4rk@users.noreply.github.com>
2018-03-07 11:19:26 +0000

    Added $mail_host variable

--- alerts/send_sql_reports.pl
+++ alerts/send_sql_reports.pl
@@ -22,6 +22,7 @@ my $writing_report = 0;
 my $report_fh;
 my @report_filenames;
 my $msg;
+my $mail_host='mail_host';
 #
 # Get the reports in one file
 #
@@ -67,4 +68,4 @@ while( my $line = <$fh>)  {
 close $fh;
 close $report_fh;
 
-$msg->send( );
+$msg->send('smtp', $mail_host);
